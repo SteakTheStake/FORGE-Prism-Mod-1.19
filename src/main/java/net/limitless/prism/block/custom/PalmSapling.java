8394b568e3ce7a5f9a3442ed8fba4f646cfd5046
SteakTheStake <gabecorr25@gmail.com>
2022-09-19 21:48:47 -0700

    updated a few blocks

--- src/main/java/net/limitless/prism/block/custom/PalmSapling.java
+++ src/main/java/net/limitless/prism/block/custom/PalmSapling.java
@@ -1,9 +1,24 @@
 package net.limitless.prism.block.custom;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class PalmSapling extends Block {
     public PalmSapling(Properties properties) {
         super(properties);
     }
+
+
+    private static final VoxelShape SHAPE =
+            Block.box(6, 8, 6, 6, 16, 6);
+
+    @Override
+    public VoxelShape getShape(BlockState p_60555_, BlockGetter p_60556_, BlockPos p_60557_, CollisionContext p_60558_) {
+        return SHAPE;
+    }
+
 }
